e60a3bf7f2742aed67a9f372f8bac636804d659a
David Sehr <sehr@google.com>
2020-10-29 14:24:29 +0100

    msm8937-common: Use dex2oat64
    Enable dex2oat64 usage on device
    
    Bug: 153380900
    Test: boot and install an application.
    Change-Id: I4582c88c8fcdb236ec020c2a09fc2ac0a705f6e2

--- properties.mk
+++ properties.mk
@@ -91,6 +91,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat64.enabled=true \
     dalvik.vm.heapstartsize=16m \
     dalvik.vm.heapgrowthlimit=256m \
     dalvik.vm.heapsize=512m \
