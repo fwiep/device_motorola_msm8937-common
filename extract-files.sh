moto-msm89xx/android_device_motorola_msm8937-common

msm8937-common: Switch to patchelf from tools-lineage
Change-Id: If1a7328804fb9bc39ccec70690f1090f2d0509b3

extract-files.sh
@@ -50,7 +50,7 @@ fi
 function blob_fixup() {
     case "${1}" in
     lib64/libwfdnative.so)
-        patchelf --remove-needed android.hidl.base@1.0.so "${2}"
+        "${PATCHELF}" --remove-needed android.hidl.base@1.0.so "${2}"
         ;;
 
     product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml | product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
@@ -58,23 +58,23 @@ function blob_fixup() {
         ;;
 
     vendor/bin/charge_only_mode)
-        patchelf --add-needed libcharging_shim.so "${2}"
+        "${PATCHELF}" --add-needed libcharging_shim.so "${2}"
         ;;
 
     vendor/lib/hw/activity_recognition.msm8937.so | vendor/lib64/hw/activity_recognition.msm8937.so)
-        patchelf --set-soname activity_recognition.msm8937.so "${2}"
+        "${PATCHELF}" --set-soname activity_recognition.msm8937.so "${2}"
         ;;
 
     vendor/lib/hw/camera.msm8937.so)
-        patchelf --set-soname camera.msm8937.so "${2}"
+        "${PATCHELF}" --set-soname camera.msm8937.so "${2}"
         ;;
 
     vendor/lib64/hw/gatekeeper.msm8937.so)
-        patchelf --set-soname gatekeeper.msm8937.so "${2}"
+        "${PATCHELF}" --set-soname gatekeeper.msm8937.so "${2}"
         ;;
 
     vendor/lib64/hw/keystore.msm8937.so)
-        patchelf --set-soname keystore.msm8937.so "${2}"
+        "${PATCHELF}" --set-soname keystore.msm8937.so "${2}"
         ;;
 
     vendor/lib/libmmcamera2_sensor_modules.so)
@@ -90,7 +90,7 @@ function blob_fixup() {
         ;;
 
     vendor/lib64/libril-qc-hal-qmi.so | vendor/lib64/libril-qc-qmi-1.so)
-        patchelf --add-needed libcutils_shim.so "${2}"
+        "${PATCHELF}" --add-needed libcutils_shim.so "${2}"
         ;;
     esac
 }

fwiep/device_motorola_msm8937-common

extract-files.sh
@@ -50,7 +50,7 @@ fi
 function blob_fixup() {
     case "${1}" in
     lib64/libwfdnative.so)
-        patchelf --remove-needed android.hidl.base@1.0.so "${2}"
+        "${PATCHELF}" --remove-needed android.hidl.base@1.0.so "${2}"
         ;;
 
     product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml | product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
@@ -59,27 +59,27 @@ function blob_fixup() {
 
     # memset shim
     vendor/bin/charge_only_mode)
-        patchelf --add-needed libmemset_shim.so "${2}"
+        "${PATCHELF}" --add-needed libmemset_shim.so "${2}"
         ;;
 
     vendor/lib/hw/activity_recognition.msm8937.so | vendor/lib64/hw/activity_recognition.msm8937.so)
-        patchelf --set-soname activity_recognition.msm8937.so "${2}"
+        "${PATCHELF}" --set-soname activity_recognition.msm8937.so "${2}"
         ;;
 
     vendor/lib/hw/camera.msm8937.so)
-        patchelf --set-soname camera.msm8937.so "${2}"
+        "${PATCHELF}" --set-soname camera.msm8937.so "${2}"
         ;;
 
     vendor/lib64/hw/gatekeeper.msm8937.so)
-        patchelf --set-soname gatekeeper.msm8937.so "${2}"
+        "${PATCHELF}" --set-soname gatekeeper.msm8937.so "${2}"
         ;;
 
     vendor/lib64/hw/keystore.msm8937.so)
-        patchelf --set-soname keystore.msm8937.so "${2}"
+        "${PATCHELF}" --set-soname keystore.msm8937.so "${2}"
         ;;
 
     vendor/lib/libactuator_dw9767_truly.so)
-        patchelf --set-soname libactuator_dw9767_truly.so "${2}"
+        "${PATCHELF}" --set-soname libactuator_dw9767_truly.so "${2}"
         ;;
 
     # Fix camera recording
@@ -98,6 +98,13 @@ function blob_fixup() {
     vendor/lib64/libmdmcutback.so)
         sed -i "s|libqsap_sdk.so|libqsapshim.so|g" "${2}"
         ;;
+<<<<<<< HEAD
+=======
+
+    vendor/lib64/libril-qc-qmi-1.so)
+        "${PATCHELF}" --add-needed "libcutils_shim.so" "${2}"
+        ;;
+>>>>>>> 401b968... msm8937-common: Switch to patchelf from tools-lineage
     esac
 }
 