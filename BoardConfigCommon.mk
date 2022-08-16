c3b8003b6f176ccc3752cf0cd31b7111ae7c2dea
Bruno Martins <bgcngm@gmail.com>
2022-08-16 23:16:55 +0200

    msm8937-common: Drop default DRM service
    This is not needed, since Widevine DRM and Clearkey DRM
    services are available.
    
    Signed-off-by: 5H4ND <miqueiasantos20@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -67,9 +67,6 @@ DEXPREOPT_GENERATE_APEX_IMAGE := true
 BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
 BOARD_NO_CHARGER_LED := true
 
-# DRM
-TARGET_ENABLE_MEDIADRM_64 := true
-
 # FM
 BOARD_HAVE_QCOM_FM := true
 TARGET_QCOM_NO_FM_FIRMWARE := true
