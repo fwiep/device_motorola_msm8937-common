7cdf1b6bed3ab74c53e8bf5fc8a633c02f844950
5H4ND <miqueiasantos20@gmail.com>
2022-12-21 00:07:31 -0300

    msm8937-common: Nuke hannah-common in extract-files

--- extract-files.sh
+++ extract-files.sh
@@ -87,7 +87,6 @@ function blob_fixup() {
 CLEAN_VENDOR=true
 
 ONLY_BOARD_COMMON=
-ONLY_DEVICE_COMMON=
 ONLY_TARGET=
 KANG=
 SECTION=
@@ -96,9 +95,6 @@ while [ "${#}" -gt 0 ]; do
     case "${1}" in
         --only-board-common )
                 ONLY_BOARD_COMMON=true
-                ;;
-        --only-device-common )
-                ONLY_DEVICE_COMMON=true
                 ;;
         --only-target )
                 ONLY_TARGET=true
@@ -124,22 +120,14 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
-if [ -z "${ONLY_TARGET}" ] && [ -z "${ONLY_DEVICE_COMMON}" ]; then
+if [ -z "${ONLY_TARGET}" ]; then
     # Initialize the helper
     setup_vendor "${BOARD_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
 
     extract "${MY_DIR}/proprietary-files.txt" "${SRC}" ${KANG} --section "${SECTION}"
 fi
 
-if [ -z "${ONLY_BOARD_COMMON}" ] && [ -z "${ONLY_TARGET}" ] && [ -s "${MY_DIR}/../${DEVICE_COMMON}/proprietary-files.txt" ];then
-    # Reinitialize the helper for device common
-    source "${MY_DIR}/../${DEVICE_COMMON}/extract-files.sh"
-    setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
-
-    extract "${MY_DIR}/../${DEVICE_COMMON}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
-fi
-
-if [ -z "${ONLY_BOARD_COMMON}" ] && [ -z "${ONLY_DEVICE_COMMON}" ] && [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
+if [ -z "${ONLY_BOARD_COMMON}" ] && [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
     # Reinitialize the helper for device
     source "${MY_DIR}/../${DEVICE}/extract-files.sh"
     setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
