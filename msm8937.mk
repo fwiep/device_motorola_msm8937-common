54d95810a529cb26c75d9c045409c460dab82a1b
5H4ND <miqueiasantos20@gmail.com>
2022-12-21 00:05:52 -0300

    msm8937-common: Update DRM + Widevine from Coral 12/SQ1A.220105.002/79611

--- msm8937.mk
+++ msm8937.mk
@@ -134,8 +134,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3.vendor \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm@1.4.vendor \
+    android.hardware.drm-service.clearkey
 
 # Bluetooth
 PRODUCT_PACKAGES += \
